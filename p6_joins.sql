a6056c7fcbc1ad9e344a301cb19a22eeac9678a4
Paul Williams <pauliorandall@gmail.com>
2023-10-09 12:06:45 +0100

    Update p6_joins.sql

--- p6_joins.sql
+++ p6_joins.sql
@@ -22,15 +22,15 @@ SELECT
 	em.termination_type,
 	tr.description AS termination_reason
 FROM employees                AS e
-LEFT JOIN genders             AS ge ON e.fk_gender_id = ge.id
-LEFT JOIN sexual_orientations AS so ON e.fk_sexual_orientation_id = so.id
-LEFT JOIN races               AS r ON e.fk_race_id = r.id
-LEFT JOIN education_types     AS ed ON e.fk_education_type_id = ed.id
-LEFT JOIN marital_statuses    AS ms ON e.fk_marital_status_id = ms.id
+LEFT JOIN genders             AS ge  ON e.fk_gender_id = ge.id
+LEFT JOIN sexual_orientations AS so  ON e.fk_sexual_orientation_id = so.id
+LEFT JOIN races               AS r   ON e.fk_race_id = r.id
+LEFT JOIN education_types     AS ed  ON e.fk_education_type_id = ed.id
+LEFT JOIN marital_statuses    AS ms  ON e.fk_marital_status_id = ms.id
 LEFT JOIN work_location_types AS wlt ON e.fk_work_location_type_id = wlt.id
-LEFT JOIN employment_statuses AS es ON e.fk_employment_status_id = es.id
-LEFT JOIN departments         AS d ON e.fk_department_id = d.id
-LEFT JOIN job_levels          AS jl ON e.fk_job_level_id = jl.id
-LEFT JOIN employments         AS em ON e.employee_id = em.fk_employee_id
-LEFT JOIN termination_reasons AS tr ON em.fk_termination_reason_id = tr.id
+LEFT JOIN employment_statuses AS es  ON e.fk_employment_status_id = es.id
+LEFT JOIN departments         AS d   ON e.fk_department_id = d.id
+LEFT JOIN job_levels          AS jl  ON e.fk_job_level_id = jl.id
+LEFT JOIN employments         AS em  ON e.employee_id = em.fk_employee_id
+LEFT JOIN termination_reasons AS tr  ON em.fk_termination_reason_id = tr.id
 ORDER BY e.employee_id;
